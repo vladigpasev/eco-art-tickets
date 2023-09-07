15f9d8b5328e39a94e0594681d13fa1e7fcef5f2
Vladimir Pasev <vladimir.pasev@lazykey.com>
2023-09-07 20:46:30 +0300

    Ready to deploy to server

---
+++ src/app/api/events/get/route.tsx
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { connectToDatabase } from '../../../../utils/Mongo';
+import { ObjectId } from 'mongodb';
+
+export async function GET(req: NextRequest): Promise<NextResponse> {
+  const { searchParams } = new URL(req.url)
+
+const event_id = searchParams.get('event_id');
+    try {
+        // Connect to MongoDB
+        const db = await connectToDatabase();
+
+        // Fetch the events from the 'events' collection
+        const eventsCollection = db.collection('events');
+        {/* @ts-ignore */}
+        const events = await eventsCollection.findOne({ '_id': new ObjectId(event_id) });
+
+        // Create a new response with headers
+        return new NextResponse(JSON.stringify({ events }), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+    } catch (err) {
+        console.error('Failed to fetch data:', err);
+        return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+    }
+}
